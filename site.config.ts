b9bb7d94c0f200ec92ff398efe71cfacbc06a125
klazarenko <ki_lazarenko@icloud.com>
2026-09-05 03:28:17 +0300

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -1,64 +1,32 @@
-import { siteConfig } from './lib/site-config'
+export default {
+  // Имя вашего сайта и автора
+  name: 'Notion Site',
+  author: 'klazarenko',
 
-export default siteConfig({
-  // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  // ID вашей корневой публичной страницы Notion (обязательно)
+  rootNotionPageId: '003b7f41879c8342844a01bd2bf8de37',
 
-  // if you want to restrict pages to a single notion workspace (optional)
-  // (this should be a Notion ID; see the docs for how to extract this)
-rootNotionPageId: '003b7f41879c8342844a01bd2bf8de37',
-  // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  // Минимальные базовые настройки шаблона
+  domain: 'nextjs-notion-starter-kit.vercel.app',
+  description: 'My Notion Site',
+  isReadme: false,
+  isPreviewImageSupportEnabled: false,
+  isTweetEmbedSupportEnabled: false,
+  isSearchEnabled: false,
 
-  // open graph metadata (optional)
-  description: 'Example Next.js Notion Starter Kit Site',
+  // Ссылки на социальные сети (можно оставить пустыми)
+  twitter: null,
+  github: null,
+  linkedin: null,
+  mastodon: null,
+  newsletter: null,
+  youtube: null,
 
-  // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
-  // mastodon: '#', // optional mastodon profile URL, provides link verification
-  // newsletter: '#', // optional newsletter URL
-  // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
-
-  // default notion icon and cover images for site-wide consistency (optional)
-  // page-specific values will override these site-wide defaults
+  // Настройки отображения блоков
   defaultPageIcon: null,
   defaultPageCover: null,
   defaultPageCoverPosition: 0.5,
 
-  // whether or not to enable support for LQIP preview images (optional)
-  isPreviewImageSupportEnabled: true,
-
-  // whether or not redis is enabled for caching generated preview images (optional)
-  // NOTE: if you enable redis, you need to set the `REDIS_HOST` and `REDIS_PASSWORD`
-  // environment variables. see the readme for more info
-  isRedisEnabled: false,
-
-  // map of notion page IDs to URL paths (optional)
-  // any pages defined here will override their default URL paths
-  // example:
-  //
-  // pageUrlOverrides: {
-  //   '/foo': '067dd719a912471ea9a3ac10710e7fdf',
-  //   '/bar': '0be6efce9daf42688f65c76b89f8eb27'
-  // }
-  pageUrlOverrides: null,
-
-  // whether to use the default notion navigation style or a custom one with links to
-  // important pages. To use `navigationLinks`, set `navigationStyle` to `custom`.
+  // Настройки навигации
   navigationStyle: 'default'
-  // navigationStyle: 'custom',
-  // navigationLinks: [
-  //   {
-  //     title: 'About',
-  //     pageId: 'f1199d37579b41cbabfc0b5174f4256a'
-  //   },
-  //   {
-  //     title: 'Contact',
-  //     pageId: '6a29ebcb935a4f0689fe661ab5f3b8d1'
-  //   }
-  // ]
-})
+}
