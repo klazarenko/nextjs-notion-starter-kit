489fa49c6ff7e70efad1af1d285a3682d15d5d44
klazarenko <ki_lazarenko@icloud.com>
2026-09-05 04:48:25 +0300

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -1,16 +1,15 @@
 export default {
+  // Настройки вашего личного бренда
   name: 'Notion Site',
   author: 'klazarenko',
   domain: 'lazarenko-portfolio.ru',
   description: 'My Notion Site',
 
+  // Главный ID вашей публичной страницы Notion
   rootNotionPageId: '003b7f41879c8342844a01bd2bf8de37',
 
+  // Базовые безопасные параметры отображения
   isReadme: false,
   isSearchEnabled: false,
-
-  // Это отключит фоновое зависание серверов
-  isPreviewImageSupportEnabled: false,
-  isTweetEmbedSupportEnabled: false,
-  navigationStyle: 'default'
+  isPreviewImageSupportEnabled: false
 }
