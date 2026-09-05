5e75834f027a113073aa03fd7b0062203aa80972
klazarenko <ki_lazarenko@icloud.com>
2026-09-05 03:33:38 +0300

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -1,32 +1,14 @@
 export default {
-  // Имя вашего сайта и автора
+  // Настройки автора и домена
   name: 'Notion Site',
   author: 'klazarenko',
-
-  // ID вашей корневой публичной страницы Notion (обязательно)
-  rootNotionPageId: '003b7f41879c8342844a01bd2bf8de37',
-
-  // Минимальные базовые настройки шаблона
   domain: 'nextjs-notion-starter-kit.vercel.app',
   description: 'My Notion Site',
+
+  // Главный ID вашей публичной страницы Notion
+  rootNotionPageId: '003b7f41879c8342844a01bd2bf8de37',
+
+  // Базовые параметры отображения
   isReadme: false,
-  isPreviewImageSupportEnabled: false,
-  isTweetEmbedSupportEnabled: false,
-  isSearchEnabled: false,
-
-  // Ссылки на социальные сети (заменили null на пустые строки '')
-  twitter: '',
-  github: '',
-  linkedin: '',
-  mastodon: '',
-  newsletter: '',
-  youtube: '',
-
-  // Настройки отображения блоков
-  defaultPageIcon: '',
-  defaultPageCover: '',
-  defaultPageCoverPosition: 0.5,
-
-  // Настройки навигации
-  navigationStyle: 'default'
+  isSearchEnabled: false
 }
